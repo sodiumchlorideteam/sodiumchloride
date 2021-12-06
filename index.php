a1fb0009701cd167f62c1be2982e7b938655eb27
Marimuthu <marimuthu04032002@gmail.com>
2021-12-06 19:07:01 +0530

    index.php created

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package sodium chloride.Data science and ML exploring Saas.
+ * @author Marimuthu @ www.sodiumchloride.in , Mukila @ www.sodiumchloride.in.
+ * @version beta 0.1.0
+ * @return developer contact marimuthu04032002@gmail.com
+ * @return ♥ from Thoothukudi,Tamilnadu,India.
+ * @return true
+ */
